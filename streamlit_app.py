029cb85e5ae25c6ecf1876c77204777765ad8bc8
vitkanurazizah <nurazizahvitka21@gmail.com>
2026-05-22 14:15:16 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,6 @@ st.title("🎈 sudah terbiasa tante app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+import streamlit as st
+
+st.text("This is text\n[and more text](that's not a Markdown link).")
