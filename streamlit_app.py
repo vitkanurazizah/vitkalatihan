d1d9cf69f8003764ecfdc353a359130803bfe05b
vitkanurazizah <nurazizahvitka21@gmail.com>
2026-05-22 14:26:08 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,35 +4,3 @@ st.title("🎈 sudah terbiasa tante app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
-import streamlit as st
-
-st.text("This is text\n[and more text](that's not a Markdown link).")
-import streamlit as st
-
-st.audio("cat-purr.mp3", format="audio/mpeg", loop=True)
-import streamlit as st
-
-st.title("apa hayo")
-st.title("_Streamlit_ is :blue[cool] :sunglasses:")
-import streamlit as st
-
-st.badge("New")
-st.badge("Success", icon=":material/check:", color="green")
-
-st.markdown(
-    ":violet-badge[:material/star: Favorite] :orange-badge[⚠️ Needs review] :gray-badge[Deprecated]"
-)
-st.badge("New")
-st.badge("Success", icon=":material/check:", color="green")
-
-st.markdown(
-    ":violet-badge[:material/star: Favorite] :orange-badge[⚠️ Needs review] :gray-badge[Deprecated]"
-)
-mport streamlit as st
-
-st.badge("New")
-st.badge("Success", icon=":material/check:", color="green")
-
-st.markdown(
-    ":violet-badge[:material/star: Favorite] :orange-badge[⚠️ Needs review] :gray-badge[Deprecated]"
-)
