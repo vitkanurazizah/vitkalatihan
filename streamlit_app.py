7ba09e14019bcdb90e8db429eb976487e0a892ce
vitkanurazizah <nurazizahvitka21@gmail.com>
2026-05-22 14:21:16 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,3 +14,11 @@ import streamlit as st
 
 st.title("apa hayo")
 st.title("_Streamlit_ is :blue[cool] :sunglasses:")
+import streamlit as st
+
+st.badge("New")
+st.badge("Success", icon=":material/check:", color="green")
+
+st.markdown(
+    ":violet-badge[:material/star: Favorite] :orange-badge[⚠️ Needs review] :gray-badge[Deprecated]"
+)
